9545a9f78e3946c1ee21b2c7bb67dd4462372658
Bryan Rhodunda <rhodunda@udel.edu>
2020-10-15 17:29:20 -0400

    boiler plate part 1

---
+++ src/hoc/layout/Layout.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import Aux from '../Aux/Aux'
+
+
+class Layout extends Component {
+    render () {
+        return(
+            <Aux>
+                
+            </Aux>
+        )
+    }
+}
+
+export default Layout
