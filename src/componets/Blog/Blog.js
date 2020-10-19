1f8908d6579bf2dda0574e11fd9143d1199f3c99
Bryan Rhodunda <rhodunda@udel.edu>
2020-10-18 20:52:10 -0400

    added blog element

---
+++ src/componets/Blog/Blog.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from './Blog.module.css'
+
+const Blog = () => (
+<div>design the blog cards here</div>
+<div>change name to BlogCards</div>
+)
+
+
+export default Blog
