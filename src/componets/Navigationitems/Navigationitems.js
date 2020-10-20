fc13292a11989205997d55cf4c973543e31974df
Bryan Rhodunda <rhodunda@udel.edu>
2020-10-19 20:00:25 -0400

    changed the font

--- src/componets/Navigationitems/Navigationitems.js
+++ src/componets/Navigationitems/Navigationitems.js
@@ -6,7 +6,7 @@ import NavigationItem from '../Navigationitem/NavigationItem'
 
 const navigationItems = () => (
     <ul className={styles.NavItems}>
-        <h3>Bryan Rhodunda</h3>
+        <h3 className={styles.name}>Bryan Rhodunda</h3>
         <NavigationItem>Projects</NavigationItem>
         <NavigationItem>GitHub</NavigationItem>
         <NavigationItem>Contact</NavigationItem>
