6afcd7c77bb473b98149c717632cb2c5c41de453
Bryan Rhodunda <rhodunda@udel.edu>
2020-10-20 23:57:53 -0400

    added contact section

---
+++ src/componets/Contact/Contact.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const ContactInfo = {
+    number: '302-690-9994',
+    email: 'rhodunda@udel.edu'
+}
+
+const Contact = () => (
+    <div></div>
+)
+
+
+export default Contact
