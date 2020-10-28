022c18aaf490908363e86140cf47b39c04867189
Bryan Rhodunda <rhodunda@udel.edu>
2020-10-28 17:30:41 -0400

    updated layout complete

--- src/componets/Blog/BlogCards.js
+++ src/componets/Blog/BlogCards.js
@@ -4,7 +4,8 @@ import styles from './BlogCards.module.css'
 
 const BlogCards = (props) => (
 
-<div className={styles.Blog}>Blog's
+<div className={styles.Blog}>
+    <bold className={styles.idTab}>Blogs</bold>
     <div className={styles.BlogDes}>name of Blog</div>
     <img src={props.img} alt="Avator" />
     <div className={styles.BlogDes}>description of blogs</div>
