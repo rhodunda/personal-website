1095d711b349082f2e184144d9ea7dc28c40cb62
Bryan Rhodunda <rhodunda@udel.edu>
2020-11-12 23:24:57 -0500

    done basic setup of store

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import React, { Component } from 'react';
 // import { connect } from 'react-redux'
+// import asyncComponent from './hoc/asyncComponent/asyncComponent'
 // import styles from './App.css'
 import Layout from './hoc/layout/Layout'
 import './App.css';
+
+
+// const asyncBlogs = asyncComponent(() => {
+//   return import('./containers/Blogs/Blogs')
+// })
 
 class App extends Component {
   render () {
--- src/index.js
+++ src/index.js
@@ -1,15 +1,40 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, compose, combineReducers } from 'redux';
+import thunk from 'redux-thunk';
+
 import './index.css';
 import App from './App';
+// import registerServiceWorker from './registerServiceWorker';
 import * as serviceWorker from './serviceWorker';
+import blogReducer from './store/reducers/blog'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+
+// inable to block Redux Dev tools
+// const composeEnhancers = process.env.NODE_ENV === 'development' ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ : null || compose;
+const composeEnhancers =  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__  || compose;
+
+
+const rootReducer = combineReducers({
+  blog: blogReducer
+})
+
+const store = createStore(rootReducer, composeEnhancers(
+  applyMiddleware(thunk)
+))
+
+const app = (
+  <Provider store={store}>
+  <BrowserRouter>
+  <App/>
+  </BrowserRouter>
+  </Provider>
+)
+
+ReactDOM.render( app, document.getElementById( 'root'))
+// registerServiceWorker();
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
