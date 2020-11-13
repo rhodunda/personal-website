aaed5d9081e2e9c165645f8433e0d2f01c15ae56
Bryan Rhodunda <rhodunda@udel.edu>
2020-11-13 14:07:30 -0500

    added blogs reducer

--- src/store/reducers/blog.js
+++ src/store/reducers/blog.js
@@ -1,7 +1,34 @@
-
+import * as actionTypes from '../actions/actionTypes'
+import { updateObject } from '../../shared/utility'
 
 
 const initialState = {
     blogs:[],
     loading: false
 }
+
+const fetchBlogStart = (state, action) => {
+    return updateObject( state, { loading: true })
+}
+
+
+const fetchBlogSuccess = (state, action) => {
+    return updateObject( state, {
+        blogs: action.blogs,
+        loading: false
+    })
+}
+
+const fetchBlogFail = (state, action) => {
+    return updateObject( state, { loading: false})
+}
+
+const reducer = (state = initialState, action) => {
+    switch ( action.type ) {
+        case actionTypes.FETCH_BLOGS_START: return fetchBlogStart(state, action);
+        case actionTypes.FETCH_BLOGS_SUCCESS: return fetchBlogSuccess(state, action);
+        case actionTypes.FETCH_BLOGS_FAIL: return fetchBlogFail(state, action)
+    }
+}
+
+export default reducer
