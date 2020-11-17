c3a76defaefdc8dfa1c3e2b353638d2f07de2770
Bryan Rhodunda <rhodunda@udel.edu>
2020-11-17 16:11:31 -0500

    added descriptions to projects

--- src/componets/About/About.js
+++ src/componets/About/About.js
@@ -5,14 +5,14 @@ const About = () => (
     <div className={styles.AboutMain}>
         <h3>About</h3>
         <h4 className={styles.AboutSec}>Bryan Rhodunda</h4>
-        <h4>I am a full stack web developer with a natural affinity 
+        <h4 className={styles.Description}>I am a full stack web developer with a natural affinity 
     for learning and experimenting to find creative solutions to 
     complex problems. I have experience in Ruby on Rails and JavaScript, and React/Redux
     based programming. Possess strong skills in project management and 
     application concept designs that help companies better plan and create
      modern user-friendly business solutions.</h4>
         <p className={styles.AboutSec}>Ruby On Rails, JavaScript, React</p>
-        <h4>intrest:
+        <h4 className={styles.Intrest}>intrest:
             Programming,
             Video Games,
             traveling,
--- src/componets/ProjectCards/ProjectCards.js
+++ src/componets/ProjectCards/ProjectCards.js
@@ -12,10 +12,10 @@ import ComingSoon from '../../assets/images/Coming_Soon.png'
 class  ProjectCards extends Component {
     
     state = [
-        {name: 'The Mix', img: TheMixPicture , description: 'bah bah bah bah', link: 'https://cocktailfrontend.herokuapp.com/'},
-        {name: 'Burger Builder', img: BurgerBuilderPicture, description: 'bah bah bah bah', link: 'https://burger-builder-83a64.web.app/'},
-        {name: 'Video Game Trader', img: TradeTradeCityPicture, description: 'bah bah bah bah', link: undefined},
-        {name: 'Coming Soon', img: ComingSoon, description: 'bah bah bah bah', link: undefined}
+        {name: 'The Mix', img: TheMixPicture , description: 'The Mix is an online cocktail sharing website that allows users to create, favorite, rate, and review cocktails. After a user creates an account and login they can browse through hundreds of cocktails added to the database.', link: 'https://cocktailfrontend.herokuapp.com/'},
+        {name: 'Burger Builder', img: BurgerBuilderPicture, description: 'The Burger Builder is an online e-commerce platform template for local burger restaurants. A user can customize their burger, create and account, and view past orders.', link: 'https://burger-builder-83a64.web.app/'},
+        {name: 'Video Game Trader', img: TradeTradeCityPicture, description: 'Trade City is a online trading app that allows users to trade video games from user to user. User can browse over 100 games seeded to the data base. You can add a game or platdform to your collection and offer them up for trade. Along with all that we have advanced stats about ever game, including genre, release date, rating, completion time, and more!', link: undefined},
+        {name: 'Coming Soon!!!', img: ComingSoon, description: 'SpaceX Dashboard, please check back for updates.', link: undefined}
     ]
 
     render () {
