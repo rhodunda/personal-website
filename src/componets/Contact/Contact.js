c48258da47288cdf721e5a6d6937b5ea7b8c0403
Bryan Rhodunda <rhodunda@udel.edu>
2020-11-16 20:28:40 -0500

    added profile Picture

--- src/componets/Contact/Contact.js
+++ src/componets/Contact/Contact.js
@@ -1,16 +1,18 @@
 import React from 'react'
 import styles from './Contact.module.css'
+import ProfilePicture from '../../assets/images/ProfilePic.png'
 
 const ContactInfo = {
     number: '302-690-9994',
     email: 'rhodunda@udel.edu',
-    picture: 'image-url'
+    picture: ProfilePicture
 }
 
 const Contact = () => (
-<div className={styles.ContactMain}>Contact Info
+<div className={styles.ContactMain}>
+    <div>Contact Info</div>
+    <img src={ContactInfo.picture} alt="avator" className={styles.profilePic}></img>
     <div className={styles.ContactSec}>{ContactInfo.number}</div>
-    <div >{ContactInfo.picture}</div>
     <div className={styles.ContactSec}>{ContactInfo.email}</div>
 </div>
     
