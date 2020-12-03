640eb120efc05cd0a96e1df92f122af588400a66
Bryan Rhodunda <rhodunda@udel.edu>
2020-12-03 14:11:44 -0500

    updated

--- src/componets/Navigationitem/NavigationItem.js
+++ src/componets/Navigationitem/NavigationItem.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import styles from './Navigationitem.module.css'
-import linkedin from '../../assets/images/linkedin.png'
-import medium from '../../assets/images/meduim.png'
+// import linkedin from '../../assets/images/linkedin.png'
+// import medium from '../../assets/images/meduim.png'
 
 const navigationItem = (props) => (
     <a  href={props.link}className={styles.navLink} target="_blank" rel="noopener noreferrer" >
