4a9f5a55c30a7a3c030db809d1f05125441fe38f
Bryan Rhodunda <rhodunda@udel.edu>
2020-12-03 16:43:49 -0500

    updated

--- src/hoc/layout/Layout.js
+++ src/hoc/layout/Layout.js
@@ -7,7 +7,7 @@ import Blogs from '../../containers/Blogs/Blogs'
 import About from '../../componets/About/About'
 import Contact from '../../componets/Contact/Contact'
 import GitHub from '../../componets/Github/GitHub'
-import Picture from '../../componets/Picture/Pictures'
+// import Picture from '../../componets/Picture/Pictures'
 
 
 class Layout extends Component {
