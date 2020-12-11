752f6cbbe9ab8974fcf63530e226e870f7515c62
Bryan Rhodunda <rhodunda@udel.edu>
2020-12-10 19:10:25 -0500

    added heroku alert message

--- src/componets/ProjectCards/ProjectCards.js
+++ src/componets/ProjectCards/ProjectCards.js
@@ -24,10 +24,10 @@ import ComingSoon from '../../assets/images/Coming_Soon.png'
 class  ProjectCards extends Component {
     
     state = [
-        {name: 'The Mix', img1: TheMixPicture, img2: TheMixPicture2, img3: TheMixPicture3, description: 'The Mix is the perfect place to find your next cocktail. Using an external api to source existing cocktails and the giving each user the ability to create their own cocktail, The Mix is the perfect spot to share and find cocktails.', link: 'https://cocktailfrontend.herokuapp.com/'},
-        {name: 'Burger Builder', img1: BurgerBuilderPicture, img2: BurgerBuilderPicture2, img3: BurgerBuilderPicture3, description: 'The Burger Builder is an online e-commerce platform template for local burger restaurants. A user can customize their burger, create and account, and view past orders.', link: 'https://burger-builder-83a64.web.app/'},
-        {name: 'Video Game Trader', img1: TradeTradeCityPicture, img2: TradeTradeCityPicture2, img3: TradeTradeCityPicture3, description: 'Trade City is a online trading app that allows users to trade video games from user to user. User can browse over 100 games seeded to the data base. You can add a game or platdform to your collection and offer them up for trade. Along with all that we have advanced stats about ever game, including genre, release date, rating, completion time, and more!', link: undefined},
-        {name: 'Coming Soon!!!', img1: ComingSoon, img2: ComingSoon, img3: ComingSoon, description: 'SpaceX Dashboard, please check back for updates.', link: undefined}
+        {name: 'The Mix', img1: TheMixPicture, img2: TheMixPicture2, img3: TheMixPicture3, description: 'The Mix is the perfect place to find your next cocktail. Using an external api to source existing cocktails and the giving each user the ability to create their own cocktail, The Mix is the perfect spot to share and find cocktails.', link: 'https://cocktailfrontend.herokuapp.com/', heroku: true},
+        {name: 'Burger Builder', img1: BurgerBuilderPicture, img2: BurgerBuilderPicture2, img3: BurgerBuilderPicture3, description: 'The Burger Builder is an online e-commerce platform template for local burger restaurants. A user can customize their burger, create and account, and view past orders.', link: 'https://burger-builder-83a64.web.app/', heroku: false},
+        {name: 'Video Game Trader', img1: TradeTradeCityPicture, img2: TradeTradeCityPicture2, img3: TradeTradeCityPicture3, description: 'Trade City is a online trading app that allows users to trade video games from user to user. User can browse over 100 games seeded to the data base. You can add a game or platdform to your collection and offer them up for trade. Along with all that we have advanced stats about ever game, including genre, release date, rating, completion time, and more!', link: undefined, heroku: false},
+        {name: 'Coming Soon!!!', img1: ComingSoon, img2: ComingSoon, img3: ComingSoon, description: 'SpaceX Dashboard, please check back for updates.', link: undefined, heroku: false}
     ]
 
     render () {
@@ -54,6 +54,7 @@ class  ProjectCards extends Component {
             descripton={project.description}
             changed={project.link}
             disabledCheck={checkAvailablilty(project)}
+            heroku={project.heroku}
             />
         ))}
             
--- src/componets/UI/ProjectCard/ProjectCard.js
+++ src/componets/UI/ProjectCard/ProjectCard.js
@@ -2,11 +2,25 @@ import React from 'react'
 import styles from './ProjectCard.module.css'
 import Button from '../../UI/Button/Button'
 import Carousel from '../../ImageCarousel/Image Carousel'
+import Alert from 'react-bootstrap/Alert'
 
 
 const ProjectCard = (props) => {
+ 
+    let alert = <p></p>
+    
+    console.log(props.name, props.heroku)
 
-
+    if(props.heroku === true){
+            alert = 
+        <Alert variant="danger">
+                <p >This projects have been deployed using Heroku's free plan.
+                    Both frontend and backend servers will take about 15 seconds to wake.
+                </p>
+        </Alert>
+    
+       
+    }
 
 return (
     <div className={styles.coupon}>
@@ -18,7 +32,8 @@ return (
         image3={props.picture_three}>
         {/* <img src={props.img} alt="Avator" className={styles.projectImg} /> */}
         </Carousel>
-        <p className={styles.container}>{props.descripton}</p>  
+        <p className={styles.container}>{props.descripton}</p> 
+        {alert} 
         <Button 
         link={props.changed} 
         disabled={!props.disabledCheck}
